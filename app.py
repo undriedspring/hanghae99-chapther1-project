a340a53f67b67c68972a640b8ee58c172c4a8237
siun <in6318008@gmail.com>
2021-11-03 19:17:19 +0900

    MainPage Change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,jsonify,request
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -13,15 +13,25 @@ db = client.hanghae99_chapter1
 def index():
     return render_template('index.html')
 
+
+# main_list
+@app.route('/main_list',methods=['GET'])
+def main_list():
+    main_list = list(db.forTheCulture.find({}, {'_id': False}))
+    return jsonify({'main_list': main_list})
+
+
 # login
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 # singUp
 @app.route('/singUp')
 def singUp():
     return render_template('singUp.html')
+
 
 # myPage
 @app.route('/mypage')
@@ -30,4 +40,4 @@ def myPage():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
--- insertTheTable.py
+++ insertTheTable.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
